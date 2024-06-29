c199172c457a4727d41368c3b309c1236679f69d
Jack Watters <johnwatt@usc.edu>
2024-06-28 18:13:10 -0700

    Types file.

---
+++ types/index.ts
@@ -0,0 +1,24 @@
+export interface PageToScrape {
+	url: string;
+	type: "client" | "server";
+	articleContainerSelector: string;
+	linkSelector: string;
+	titleSelector: string;
+	descriptionSelector: string | undefined;
+	dateSelector: string | undefined;
+	removeIfNoDate?: boolean;
+}
+
+export interface ArticleData {
+	link?: string;
+	title?: string;
+	description?: string;
+	date?: Date;
+}
+
+export interface ValidArticleData {
+	link: string;
+	title: string;
+	description?: string;
+	date?: Date;
+}
