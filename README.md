47534eace9e1111966ef8529e51f81626b9e37c8
Jack Watters <johnwatt@usc.edu>
2024-07-02 23:36:43 -0700

    Update index.

--- README.md
+++ README.md
@@ -2,7 +2,23 @@
 
 ## TODO
 
-- add error handling ie scrapeWithRetryAndDelay
+- better styling...
+  - more on theme with bcx
+- add new results to main
+
+- have claude rename all the things
+- better error handling (claude -> production nodejs app)
+- fix tests
+
+- turn into rest api
+ -> create/deploy separate example app
+
+- show dad
+- see if i can find epic api
+- cron job
+
+---
+
 - determine if a good way to group newsletter content  
 - CRON job
 
--- src/index.ts
+++ src/index.ts
@@ -1,115 +1,101 @@
 import "dotenv/config";
 
 import { promises as fs } from "node:fs";
+import path from "node:path";
 import debug from "debug";
-import Handlebars from "handlebars";
-import puppeteer, { Page } from "puppeteer";
+import puppeteer from "puppeteer";
 
-import type {
-	ArticleDisplayData,
-	ValidArticleData,
-	ValidArticleDataWithCount,
-} from "../types";
-import { initializeGenAI } from "./ai";
-import { APP_NAME, SPECIFIC_PAGES } from "./constants";
+import { fetchArticles } from "./data/data-fetching";
+import { filterAllArticles, filterPageArticles } from "./data/data-filtering";
+import { processArticles } from "./data/format-articles";
+import { renderTemplate } from "./display/template";
+import { initializeGenAI } from "./lib/ai";
+import { SPECIFIC_PAGES } from "./lib/constants";
+import { runWeekly } from "./lib/cron";
 
-import { runWeekly } from "./cron";
-import {
-	formatDescription,
-	generateJsonResponse,
-	generateStringResponse,
-	parseJsonDate,
-} from "./utils";
-
-import Bottleneck from "bottleneck";
-import displayDataJson from "../article-display-data.json" assert {
-	type: "json",
-};
-import rankedJson from "../ranked.json" assert { type: "json" };
-import responseJson from "../response.json" assert { type: "json" };
-import resultsJson from "../results.json" assert { type: "json" };
-import { filterAllArticles, filterPageArticles } from "./date-filtering";
-import { processArticles } from "./format-articles";
-import { renderTemplate, sendEmail } from "./template";
-
-const log = debug(`${APP_NAME}:index.ts`);
+const log = debug(`${process.env.APP_NAME}:index.ts`);
 
 export const model = initializeGenAI();
 
-// TODO add new results to main
-// TODO style template
+export async function generateNewsletterData() {
+	log("generating newsletter data");
 
-// TODO rank articles (combine with filter??)
-// TODO fine tune prompts
+	const browser = await puppeteer.launch();
+	try {
+		const browserPage = await browser.newPage();
 
-// TODO cron job
+		const results = [];
+		for (const page of SPECIFIC_PAGES) {
+			const articleLinks = await fetchArticles(page, browserPage);
+
+			const relevantArticles = await filterPageArticles(articleLinks, page);
+
+			results.push(...relevantArticles);
+		}
+
+		const relevantArticles = await filterAllArticles(results);
+
+		const newsletterData = await processArticles(relevantArticles, browserPage);
+
+		log("newsletter data generated");
+
+		return newsletterData;
+	} catch (error) {
+		console.error(error);
+	} finally {
+		await browser.close();
+	}
+}
+
 async function main() {
 	runWeekly(async () => {
-		const browser = await puppeteer.launch();
 		try {
-			const browserPage = await browser.newPage();
+			const newsletterData = await generateNewsletterData();
 
-			// const results = [];
-			// for (const page of SPECIFIC_PAGES) {
-			// 	const articleLinks = await fetchArticles(page, browserPage);
+			const template = await renderTemplate(newsletterData);
 
-			// 	if (!articleLinks.length) continue;
+			await fs.writeFile(
+				path.join(path.resolve(), "public", "newsletter.html"),
+				template,
+			);
 
-			// 	const relevantArticles = await filterPageArticles(articleLinks, page);
-
-			// 	results.push(...relevantArticles);
-			// }
-			// fs.writeFile("response.json", JSON.stringify(results, null, 2));
-
-			// const relevantArticles = await filterAllArticles(results);
-
-			// testing
-			// const relevantArticles = await filterAllArticles(
-			// 	parseJsonDate(responseJson),
-			// );
-			// fs.writeFile("results.json", JSON.stringify(relevantArticles, null, 2));
-
-			// const parsedResultsJson = parseJsonDate(resultsJson);
-
-			// select n most relevant articles - use ai to rank
-			// const rankedArticles = await rankArticles(parsedResultsJson);
-
-			// log(rankedArticles.length);
-			// fs.writeFile("ranked.json", JSON.stringify(rankedArticles, null, 2));
-
-			// const rankedArticles = parseJsonDate(rankedJson);
-
-			// const articleDisplayData = await processArticles(
-			// 	rankedArticles,
-			// 	browserPage,
-			// );
-
-			// template
-
-			const result = await renderTemplate(displayDataJson);
-
-			const res = await sendEmail(result);
+			// TODO
+			// const res = await sendEmail(result);
 		} catch (error) {
 			console.error(error);
-		} finally {
-			await browser.close();
 		}
 	});
 }
 
-//
-function rankArticles(
-	articles: ValidArticleDataWithCount[],
-	numberOfArticles = 30,
-	// doesnt need to omit count if it requires an extra loop
-): ValidArticleDataWithCount[] {
-	const sortedArticles = articles.sort((a, b) => b.count - a.count);
-
-	// ai shite!!!
-
-	return sortedArticles.slice(0, numberOfArticles);
-}
 main();
+
+import { Resend } from "resend";
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export async function sendEmail(html: string, to = "jack.watters@me.com") {
+	const date = new Date();
+	const formattedDate = date.toLocaleDateString("en-US", {
+		weekday: "long",
+		year: "numeric",
+		month: "long",
+		day: "numeric",
+	});
+
+	const { data, error } = await resend.emails.send({
+		from: "Yats Support <support@yatusabes.co>",
+		to,
+		subject: `Test Newsletter' - ${formattedDate}`,
+		html,
+	});
+
+	if (error) {
+		return console.error({ error });
+	}
+
+	return new Response(
+		JSON.stringify({ message: "Email sent successfully", data }),
+	);
+}
 
 // "You are a homecare business operator. You are tasked with choosing which articles to include in a newsletter. You will be provided with a list of about 200 articles and their metadata. Your job is to filter out articles that are not relevant to the topic of home health. You should reduce the list to the 30 most relevant and interesting articles. Please order the articles by relevance score, with the highest score being the first in the list. Return the filtered list of articles as a JSON array.",
 
