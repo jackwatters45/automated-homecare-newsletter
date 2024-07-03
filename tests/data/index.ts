8119970895363bdf180f761870f549e07bd115e8
Jack Watters <johnwatt@usc.edu>
2024-07-02 23:31:22 -0700

    Sample data + html.

---
+++ tests/data/index.ts
@@ -0,0 +1,11 @@
+export { default as articleDisplayData } from "./display-article-data.json" assert {
+	type: "json",
+};
+
+export { default as rawArticleData } from "./raw-article-data.json" assert {
+	type: "json",
+};
+
+export { default as filteredArticleData } from "./filtered-article-data.json" assert {
+	type: "json",
+};
